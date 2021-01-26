fcf3de407fa0a08870d8f4689dbe51c8409b4cda
yogesh16101999 <chavanyogesh981@gmail.com>
2021-01-26 20:02:29 +0530

    client added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import memories from './images/memories.png';
+import useStyles from './styles';
+import Posts from './component/Posts/Posts.js';
+import Form from './component/Form/Form.js';
+import { useDispatch } from 'react-redux';
+import { getPosts } from './actions/posts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const classes = useStyles();
+
+    const [currentId, setCurrentId] = useState(null);
+
+    const dispatch = useDispatch();
+    useEffect(() => {
+        console.log('USEEFFEXT')
+        dispatch(getPosts())
+    }, [dispatch]);
+
+    return (
+        <Container maxWidth='xl'>
+            <AppBar className={classes.appBar} position="static" color='inherit'>
+                <Typography className={classes.heading} variant="h2" align="center">
+                    memories
+                </Typography>
+                <img className={'classes.image'} src={memories} alt="memories" height="80" width='80' />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid className="mainContainer" container justify="space-between" alignItems='stretch' spacing={3} >
+                        <Grid item xs={12} sm={8}>
+                            <Posts setCurrentId={setCurrentId} />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form currentId={currentId} setCurrentId={setCurrentId} />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
+    );
 }
-
 export default App;
---
+++ src/api/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+const url = 'http://localhost:7000/posts';
+
+export const fetchPosts = () => axios.get(url);
+export const createPosts = (newPost) => axios.post(url, newPost);
+export const updatePosts = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
+export const deletePosts = (id) => axios.delete(`${url}/${id}`);
+export const likePosts = (id) => axios.patch(`${url}/${id}/likePost`);
